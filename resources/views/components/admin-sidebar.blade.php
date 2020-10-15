51610a9c1d5f83228b7b261438380c2aaa4afad2
abela <abelardhana96@gmail.com>
2020-10-15 13:59:38 +0800

    ADD | Component admin sidebar

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <!-- Sidebar outter -->
+    <div class="main-sidebar sidebar-style-2">
+        <!-- sidebar wrapper -->
+        <aside id="sidebar-wrapper">
+            <!-- sidebar brand -->
+            <div class="sidebar-brand">
+                <a href="index.html">{{ config('app.name', 'Laravel') }}</a>
+            </div>
+            <!-- sidebar menu -->
+            <ul class="sidebar-menu">
+                <!-- menu header -->
+                <li class="menu-header">Dashboard</li>
+                <!-- menu item -->
+                <li class="active">
+                    <a href="index.html"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                </li>
+            </ul>
+        </aside>
+    </div>
+</div>
