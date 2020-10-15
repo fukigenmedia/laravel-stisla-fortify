4e5b471a06d8609b9992e98f6bcad5bae4b736f0
Fjr_putra.a <42708438+fjr-putra-a@users.noreply.github.com>
2020-10-15 16:20:04 +0800

    STYLE | Forgot Password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,40 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<div>
+    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+</div>
 
-    @if (session('status'))
-        <div>
-            {{ session('status') }}
-        </div>
-    @endif
-
-    @if ($errors->any())
-        <div>
-            <div>{{ __('Whoops! Something went wrong.') }}</div>
-
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
+<div class="container mt-5">
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                <div class="login-brand">
+                    <img src="https://ui-avatars.com/api/size=100&&?name={{ config('app.name') }}" alt="logo"
+                        width="100" class="shadow-light rounded-circle">
+                </div>
 
-        <div>
-            <label>{{ __('Email') }}</label>
-            <input type="email" name="email" value="{{ old('email') }}" required autofocus />
-        </div>
+                @if (session('status'))
+                <div class="alert alert-info shadow-sm">
+                    <i class="fas fa-info fa-fw mr-2"></i>
+                    {{ session('status') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
 
-        <div>
-            <button type="submit">
-                {{ __('Email Password Reset Link') }}
-            </button>
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h4>Forgot Password</h4>
+                    </div>
+
+                    <div class="card-body">
+                        <p class="text-muted">We will send a link to reset your password</p>
+                        <form method="POST">
+                            <div class="form-group">
+                                <label for="email">{{ __('Email') }}</label>
+                                <input id="email" type="email" class="form-control" name="email" tabindex="1"
+                                    value="{{ old('email') }}" required autofocus>
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                    {{ __('Email Password Reset Link') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </form>
+</div>
 @endsection
