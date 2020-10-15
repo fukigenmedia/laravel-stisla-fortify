197cf1e85759939bfbe1bea8b3eda83fa5bb53c5
Fjr_putra.a <42708438+fjr-putra-a@users.noreply.github.com>
2020-10-15 12:56:24 +0800

    ADD | halaman error

---
+++ resources/views/errors/deleted.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', 'Deleted')
+@section('code', '401')
+
+@section('content')
+<div class="page-error mt-5">
+  <div class="page-inner">
+    <h1>DELETED</h1>
+    <div class="page-description">
+      Anda telah menghapus akun.
+      Silahkan kontak admin untuk info lebih lanjut.
+    </div>
+    <div class="page-search">
+      <div class="mt-2">
+        <a href="">Kembali ke Home</a>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
