d9f7df0a62db6d356bc50fc90cabdb3d2052b9d0
Fjr_putra.a <42708438+fjr-putra-a@users.noreply.github.com>
2020-10-15 16:27:44 +0800

    UPDATE | Foget Password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -6,49 +6,48 @@
 </div>
 
 <div class="container mt-5">
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-        <div class="row">
-            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                <div class="login-brand">
-                    <img src="https://ui-avatars.com/api/size=100&&?name={{ config('app.name') }}" alt="logo"
-                        width="100" class="shadow-light rounded-circle">
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <img src="https://ui-avatars.com/api/size=100&&?name={{ config('app.name') }}" alt="logo" width="100"
+                    class="shadow-light rounded-circle">
+            </div>
+
+            @if (session('status'))
+            <div class="alert alert-info shadow-sm">
+                <i class="fas fa-info fa-fw mr-2"></i>
+                {{ session('status') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endif
+
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Forgot Password</h4>
                 </div>
 
-                @if (session('status'))
-                <div class="alert alert-info shadow-sm">
-                    <i class="fas fa-info fa-fw mr-2"></i>
-                    {{ session('status') }}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                @endif
+                <div class="card-body">
+                    <p class="text-muted">We will send a link to reset your password</p>
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
 
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h4>Forgot Password</h4>
-                    </div>
+                        <div class="form-group">
+                            <label for="email">{{ __('Email') }}</label>
+                            <input id="email" type="email" class="form-control" name="email" tabindex="1"
+                                value="{{ old('email') }}" required autofocus>
+                        </div>
 
-                    <div class="card-body">
-                        <p class="text-muted">We will send a link to reset your password</p>
-                        <form method="POST">
-                            <div class="form-group">
-                                <label for="email">{{ __('Email') }}</label>
-                                <input id="email" type="email" class="form-control" name="email" tabindex="1"
-                                    value="{{ old('email') }}" required autofocus>
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                                    {{ __('Email Password Reset Link') }}
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                {{ __('Email Password Reset Link') }}
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </form>
+    </div>
 </div>
 @endsection
