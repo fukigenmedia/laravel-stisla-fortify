82d12c25f426c9b71eec39e74330b9606d96a8e5
abela <abelardhana96@gmail.com>
2020-10-15 12:22:09 +0800

    ADD | Menambah stisla ke layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="lang=" {{ str_replace('_', '-', app()->getLocale()) }}"">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('stisla/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('stisla/modules/fontawesome/css/all.min.css') }}">
+    <!-- Plugins -->
+    <link rel="stylesheet" href="{{ asset('stisla/modules/bootstrap-social/bootstrap-social.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('stisla/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('stisla/css/components.css') }}">
+    <!-- Page Specific CSS File -->
+    @yield('css')
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            @yield('content')
+        </section>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ asset('stisla/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/popper.js') }}"></script>
+    <script src="{{ asset('stisla/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('stisla/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('stisla/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('stisla/js/stisla.js') }}"></script>
+
+    <!-- Plugins -->
+    @yield('plugin')
+    <!-- Page Specific JS File -->
+    @yield('js')
+    <!-- Template JS File -->
+    <script src="{{ asset('stisla/js/scripts.js') }}"></script>
+    <script src="{{ asset('stisla/js/custom.js') }}"></script>
+</body>
+
 </html>
