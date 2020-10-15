0ea06a77074fd30abbec3f8e07da47c1ced0ee52
abela <abelardhana96@gmail.com>
2020-10-15 12:23:33 +0800

    ADD | yield title ke layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- General CSS Files -->
