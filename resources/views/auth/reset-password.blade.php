ac7da688eda3668af9fcc6287d3b0ddb46d21ac1
Fjr_putra.a <42708438+fjr-putra-a@users.noreply.github.com>
2020-10-15 16:26:49 +0800

    STYLE | Reset Password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,42 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-    @if ($errors->any())
-        <div>
-            <div>{{ __('Whoops! Something went wrong.') }}</div>
 
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+<div class="container mt-5">
+
+    <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <img src="https://ui-avatars.com/api/size=100&&?name={{ config('app.name') }}" alt="logo" width="100"
+                    class="shadow-light rounded-circle">
+            </div>
+
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Reset Password</h4>
+                </div>
+
+                <div class="card-body">
+                    <p class="text-muted">We will send a link to reset your password</p>
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
+
+                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                        <div class="form-group">
+                            <label for="email">{{ __('Email') }}</label>
+                            <input id="email" type="email" class="form-control" name="email" tabindex="1"
+                                value="{{ old('email', $request->email) }}" required autofocus>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">{{ __('Password') }}</label>
+                            <input id="password" type="password" class="form-control pwstrength"
+                                data-indicator="pwindicator" name="password" tabindex="2" required>
+                            <div id="pwindicator" class="pwindicator">
+                                <div class="bar"></div>
+                                <div class="label"></div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">{{ __('Confirm Password') }}</label>
+                            <input id="password_confirmation" type="password" class="form-control"
+                                name="password_confirmation" tabindex="2" required>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                {{ __('Reset Password') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <form method="POST" action="{{ route('password.update') }}">
-        @csrf
-
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <div>
-        	<label>{{ __('Email') }}</label>
-        	<input type="email" name="email" value="{{ old('email', $request->email) }}" required autofocus />
-        </div>
-
-        <div>
-            <label>{{ __('Password') }}</label>
-            <input type="password" name="password" required autocomplete="new-password" />
-        </div>
-
-        <div>
-            <label>{{ __('Confirm Password') }}</label>
-            <input type="password" name="password_confirmation" required autocomplete="new-password" />
-        </div>
-
-        <div>
-            <button type="submit">
-                {{ __('Reset Password') }}
-            </button>
-        </div>
-    </form>
+    </div>
+</div>
 @endsection
