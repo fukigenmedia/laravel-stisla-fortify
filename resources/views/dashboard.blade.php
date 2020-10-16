6e36ff4632f4f01ed84fc5ca02f99dd207f3bd7a
abela <abelardhana96@gmail.com>
2020-10-16 11:07:13 +0800

    CHANGE | Home -> Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,17 @@
         <h4>Dashboard</h4>
     </div>
     <div class="card-body">
+        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updateProfileInformation()))
+        @include('profile.update-profile-information-form')
+        @endif
+
+        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+        @include('profile.update-password-form')
+        @endif
+
+        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::twoFactorAuthentication()))
+        @include('profile.two-factor-authentication-form')
+        @endif
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ducimus temporibus aut ratione maxime
         accusamus
         commodi esse itaque tenetur. Temporibus ducimus ea veniam quisquam rerum vitae? Animi quos laudantium quam?
